77385f5602b1358ee8e6080d993328597093987e
thomas-gleizes <thomasgleizes34@gmail.com>
2021-08-21 22:11:15 +0200

    V2

---
+++ client/dist/background.js
@@ -0,0 +1,19 @@
+/* global chrome */
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.url) {
+    console.log(`URL change to ${changeInfo.url}`);
+    const { hostname, pathname, searchParams } = new URL(changeInfo.url);
+    if (
+      hostname === "www.youtube.com" &&
+      pathname === "/watch" &&
+      searchParams.get("v")
+    ) {
+      console.log("valide ID", searchParams.get("v"));
+      chrome.tabs.sendMessage(tabId, {
+        action: "info",
+        url: changeInfo.url,
+      });
+    }
+  }
+});
